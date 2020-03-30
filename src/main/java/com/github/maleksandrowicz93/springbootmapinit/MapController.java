421d133fb575a18ad932496c1a104ebd74941f9b
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-03-31 01:22:35 +0200

    correct MapController and map.html by setting parsed api data as a model attribute

--- src/main/java/com/github/maleksandrowicz93/springbootmapinit/MapController.java
+++ src/main/java/com/github/maleksandrowicz93/springbootmapinit/MapController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+
 @Controller
 public class MapController {
 
+    private Covid19Parser covid19Parser;
+
+    public MapController(Covid19Parser covid19Parser) {
+        this.covid19Parser = covid19Parser;
+    }
+
     @GetMapping
-    public String getMap(Model model) {
-        model.addAttribute("point", new Point(51.11, 17.08, "Hello everybody!"));
+    public String getMap(Model model) throws IOException {
+        model.addAttribute("points", covid19Parser.getCovidData());
         return "map";
     }
 
