e1d5cf3e00b074a0bc2ecde9e524e8519a801302
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-03-30 23:51:27 +0200

    initial commit

---
+++ src/main/java/com/github/maleksandrowicz93/springbootmapinit/SpringbootMapInitApplication.java
@@ -0,0 +1,13 @@
+package com.github.maleksandrowicz93.springbootmapinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMapInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMapInitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maleksandrowicz93/springbootmapinit/SpringbootMapInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maleksandrowicz93.springbootmapinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMapInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
