d71ced6774157bbb7c81ea9767b9d2263d2f4782
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-03-31 01:00:17 +0200

    add api data parser service

---
+++ src/main/java/com/github/maleksandrowicz93/springbootmapinit/Covid19Parser.java
@@ -0,0 +1,37 @@
+package com.github.maleksandrowicz93.springbootmapinit;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Covid19Parser {
+
+    private static final String url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    public List<Point> getCovidData() throws IOException {
+        List<Point> points = new ArrayList<>();
+        RestTemplate restTemplate = new RestTemplate();
+        String values = restTemplate.getForObject(url, String.class);
+
+        StringReader stringReader = new StringReader(values);
+        CSVParser parse = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(stringReader);
+
+        for (CSVRecord strings : parse) {
+            double lat = Double.parseDouble(strings.get("Lat"));
+            double lon = Double.parseDouble(strings.get("Long"));
+            String text = strings.get("29/15/20");
+            points.add(new Point(lat, lon, text));
+        }
+
+        return points;
+    }
+
+}
